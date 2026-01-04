b4c4533bae3d0e847771be853eb40d8a0cf72862
keerthisai A S <keerthisaias@gmail.com>
2026-01-04 09:55:08 +0530

    Add files via upload

---
+++ Multiplication_tables.py
@@ -0,0 +1,6 @@
+num=int(input("Enter the Number="))
+print("==============================")
+print(f"MULTIPLICATION TABLE FOR {num}")
+print("=============================")
+for i in range(1,101):
+    print(f"{num}x{i}={num*i}")
